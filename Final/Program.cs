c74ebe06b3529c471b1703f05afb6761c1998277
Qaece-Almahayni <qaece.almahayni@outlook.com>
2026-09-21 23:03:57 +0300

    Change student name and date of birth in Program.cs
    Updated student details in the database creation process.

--- Final/Program.cs
+++ Final/Program.cs
@@ -17,9 +17,9 @@ class Program
             // Create a new student object
             var student = new Student
             {
-                FirstName = "Mhd",
-                LastName = "Imad",
-                DateOfBirth = new DateTime(1995,11, 9)
+                FirstName = "Qaece",
+                LastName = "Mohamad",
+                DateOfBirth = new DateTime(1990,1, 12)
             };
 
             // Add the student to the Students DbSet
@@ -31,7 +31,7 @@ class Program
             Console.WriteLine("The Student is added successfully!");
 
             // Retrieve and display the student to verify
-            var savedStudent = context.Students.FirstOrDefault(s => s.FirstName == "Hanna");
+            var savedStudent = context.Students.FirstOrDefault(s => s.FirstName == "Qaece");
             if (savedStudent != null)
             {
                 Console.WriteLine($" Student: {savedStudent.FirstName} {savedStudent.LastName}");
